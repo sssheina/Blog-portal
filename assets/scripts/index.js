3959b9812eb1169dee9aaeb67e1947ffce4b93e3
Veta Sheina <sssheina@gmail.com>
2023-04-22 11:53:56 +0300

    LogIn form

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -115,6 +115,65 @@ const btnUp = {
   }
   
 
+//------------------LOGIN MODAL WINDOW---------
 
 
+function toggleSignup(){
+  document.getElementById("login-toggle").style.backgroundColor="#fff";
+   document.getElementById("login-toggle").style.color="#222";
+   document.getElementById("signup-toggle").style.backgroundColor="#65E4A3";
+   document.getElementById("signup-toggle").style.color="#fff";
+   document.getElementById("login-form").style.display="none";
+   document.getElementById("signup-form").style.display="block";
+}
 
+function toggleLogin(){
+   document.getElementById("login-toggle").style.backgroundColor="#65E4A3";
+   document.getElementById("login-toggle").style.color="#fff";
+   document.getElementById("signup-toggle").style.backgroundColor="#fff";
+   document.getElementById("signup-toggle").style.color="#222";
+   document.getElementById("signup-form").style.display="none";
+   document.getElementById("login-form").style.display="block";
+}
+
+const modalBtns = document.querySelectorAll('._modal-open');
+const modals = document.querySelectorAll('._modal');
+
+const body = document.body;
+
+function openModal(elem) {
+	elem.classList.add('_active');
+	body.classList.add('_locked')
+}
+
+function closeModal(e) {
+	if (e.target.classList.contains('modal-close') || e.target.closest('.modal-close') || e.target.classList.contains('modal-bg')) {
+		e.target.closest('._modal').classList.remove('_active');
+		body.classList.remove('_locked')
+	}
+}
+
+modalBtns.forEach(btn => {
+	btn.addEventListener('click', (e) => {
+		let data = e.target.dataset.modalOpen;
+
+		modals.forEach(modal => {
+			if (modal.dataset.modal == data || modal.dataset.modal == e.target.closest('._modal-open').dataset.modalOpen) {
+				openModal(modal)
+			}
+		})
+	})
+})
+
+modals.forEach(modal => {
+	modal.addEventListener('click', e => closeModal(e))
+})
+
+window.addEventListener('keydown', e => {
+	modals.forEach(modal => {
+		if (e.key === "Escape" && modal.classList.contains('_active')) {
+			modal.classList.remove('_active');
+			body.classList.remove('_locked');
+		}
+	})
+})
